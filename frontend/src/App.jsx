efbe78cbdf72b68c1746b2386ce4f73d8382ef50
kopi03 <navakokilan@gmail.com>
2025-06-25 11:19:01 +0530

    commit change

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -64,7 +64,7 @@ function App() {
         <main className="relative z-10">
           <Routes>
             <Route path="/" element={<HomePage />} />
-            <Route path="/spiritual-guidance" component={<SpiritualGuidance />} />
+            <Route path="/spiritual-guidance" element={<SpiritualGuidance />} />
             <Route path="/live-chat" element={<LiveChat />} />
             <Route path="/satsang" element={<Satsang />} />
             <Route path="/profile" element={<Profile />} />
