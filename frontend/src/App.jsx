39ff0b7b182fdea766cc24b58d6f717628fe9ffc
kopi03 <navakokilan@gmail.com>
2025-06-25 11:07:15 +0530

    commit 3

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import './App.css';
+import spiritualAPI from '../lib/api';
 
 // Import components (to be created)
 import HomePage from './components/HomePage';
